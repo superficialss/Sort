e081330a99e1b0eaa45abf6017c43ca18fd5672d
superficialss <superficialss@outlook.com>
2017-05-21 08:29:50 +0800

    .

--- Sort/QuitSort.h
+++ Sort/QuitSort.h
@@ -1,7 +1,9 @@
 #pragma once
+#include"InsertSort.h"
 
 int GetMidIndex(int* a, int left, int right)
 {
+	assert(a);
 	int mid = left + (right - left) >> 1;
 	if (a[left] > a[mid])
 	{
@@ -24,7 +26,7 @@ int GetMidIndex(int* a, int left, int right)
 		{
 			return mid;
 		}
-		else if (a[left] > a[mid])
+		else if (a[left] > a[right])
 		{
 			return left;
 		}
@@ -37,6 +39,7 @@ int GetMidIndex(int* a, int left, int right)
 
 int PartSort1(int* a, int left, int right)  //左右指针法
 {
+	assert(a);
 	int mid = GetMidIndex(a, left, right);
 	swap(a[mid], a[right]);
 	int key = a[right];
@@ -61,6 +64,49 @@ int PartSort1(int* a, int left, int right)  //左右指针法
 	return begin;
 }
 
+int PartSort2(int* a, int left, int right)    //挖坑法
+{
+	assert(a);
+	int mid = GetMidIndex(a, left, right);
+	swap(a[mid], a[right]);
+	int key = a[right];
+	while (left < right)
+	{
+		while (left < right && a[left] <= key)
+		{
+			++left;
+		}
+		a[right] = a[left];
+		while (left < right && a[right] >= key)
+		{
+			--right;
+		}
+		a[left] = a[right];
+	}
+	a[left] = key;
+	return key;
+}
+
+int PartSort3(int* a, int left, int right)
+{
+	assert(a);
+	int mid = GetMidIndex(a, left, right);
+	swap(a[mid], a[right]);
+	int key = a[right];
+	int prev = left - 1;
+	int cur = left;
+	while (cur < right)
+	{
+		if (a[cur] < key && ++prev != cur)
+		{
+			swap(a[prev], a[cur]);
+		}
+		++cur;
+	}
+	swap(a[++prev], a[right]);
+	return prev;
+}
+
 void QuitSort(int* a, int left, int right)
 {
 	assert(a);
@@ -68,15 +114,58 @@ void QuitSort(int* a, int left, int right)
 	{
 		return;
 	}
-	int div = PartSort1(a, left, right);
-	QuitSort(a, left, div - 1);
-	QuitSort(a, div + 1, right);
+	if (right - left < 5)
+	{
+		InsertSort(a + left, right - left + 1);
+	}
+	else
+	{
+		int div = PartSort3(a, left, right);
+		//int div = PartSort2(a, left, right);
+		//int div = PartSort1(a, left, right);
+		QuitSort(a, left, div - 1);
+		QuitSort(a, div + 1, right);
+	}
+}
+
+void QuitSortNonR(int* a, int left, int right)
+{
+	assert(a);
+	stack<int> s;
+	s.push(right);
+	s.push(left);
+	while (!s.empty())
+	{
+		int begin = s.top();
+		s.pop();
+		int end = s.top();
+		s.pop();
+		if (end - begin < 5)
+		{
+			InsertSort(a + begin, end - begin + 1);
+		}
+		else
+		{
+			int div = PartSort3(a, left, right);
+			if (div - 1 > begin)
+			{
+				s.push(div - 1);
+				s.push(begin);
+			}
+			if (div + 1 < end)
+			{
+				s.push(end);
+				s.push(div + 1);
+			}
+		}
+	}
 }
 
 void TestQuitSort()
 {
 	int a[] = { 2, 5, 4, 9, 3, 6, 8, 7, 1, 0 };
-	QuitSort(a, 0, sizeof(a) / sizeof(a[0]) - 1);
+	//QuitSort(a, 0, sizeof(a) / sizeof(a[0]) - 1);
+	QuitSortNonR(a, 0, sizeof(a) / sizeof(a[0]) - 1);
 	for (int i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
 	{
 		cout << a[i] << " ";
--- Sort/Test.cpp
+++ Sort/Test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<assert.h>
+#include<stack>
 
 using namespace std;
 
