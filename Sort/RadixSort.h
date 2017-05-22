f23d67747dd142be5b87925c157b23c85f034f43
superficialss <superficialss@outlook.com>
2017-05-22 10:00:28 +0800

    .

--- Sort/RadixSort.h
+++ Sort/RadixSort.h
@@ -19,5 +19,41 @@ void LSD(int* a, size_t n) //低位向高位排
 {
 	assert(a);
 	int digit = GetDigit(a, n);
+	int base = 1;
+	int* buket = new int[n];
+	for (size_t j = 0; j < digit; ++j)
+	{
+		int count[10] = { 0 };
+		int start[10] = { 0 };
+		for (size_t i = 0; i < n; ++i)
+		{
+			int num = (a[i] / base) % 10;
+			count[num]++;
+		}
+		for (size_t i = 1; i < 10; ++i)
+		{
+			start[i] = start[i - 1] + count[i - 1];
+		}
+		for (size_t i = 0; i < n; ++i)
+		{
+			int num = a[i]/ base % 10;
+			buket[start[num]++] = a[i];
+		}
+		for (size_t i = 0; i < n; ++i)
+		{
+			a[i] = buket[i];
+		}
+		base *= 10;
+	}	
+}
 
+void TestLSD()
+{
+	int a[] = { 2, 5, 4, 9, 3, 6, 8, 7, 1, 0 };
+	LSD(a, sizeof(a) / sizeof(a[0]));
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
 }
--- Sort/Test.cpp
+++ Sort/Test.cpp
@@ -24,7 +24,7 @@ int main()
 	//TestQuitSort();
 	//TestMergeSort();
 	//TestCountSort();
-
+	TestLSD();
 
 	return 0;
 }
